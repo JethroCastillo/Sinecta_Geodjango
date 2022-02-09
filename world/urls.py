a9faead639b5c67a0994a8720079912224a72dff
Jethro Castillo <jethrocastlewebdev@gmail.com>
2022-02-09 01:05:47 -0600

    Set Delet Predio

--- world/urls.py
+++ world/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import PredioApi
+from .views import PredioApi, PredioDetailApi, delete_view
 
 urlpatterns = [
     path('predio/', PredioApi, name='predio_list'),
+    path('predio/<int:id>/', delete_view, name='predio_list'),
     
 ]
--- world/views.py
+++ world/views.py
@@ -2,8 +2,10 @@
 # from django.contrib.gis.geos import GEOSGeometry
 # from django.contrib.gis.db.models.functions import AsGML
 
+from django.shortcuts import get_object_or_404
 from django.views.decorators.csrf import csrf_exempt
 from django.http.response import JsonResponse
+from rest_framework.generics import GenericAPIView, DestroyAPIView
 from rest_framework.parsers import JSONParser 
 from .serializers import PredioSerializer
 from .models import Predio
@@ -30,5 +32,21 @@ def PredioApi(request):
         return JsonResponse(serializer.errors, status=400)
 
 #TODO Delete, Update
+
+@csrf_exempt
+def delete_view(request, id):
+    obj = get_object_or_404(Predio, id=id)
+    if request.method == 'DELETE':
+        obj.delete()
+        return JsonResponse("success deleted", safe= False)
+
+class PredioDetailApi(GenericAPIView):
+    serializer_class = PredioSerializer
+    queryset = Predio.objects.all()
+    lookup_field = 'id'
+
+    def get_queryset(self):
+        return self.queryset.get(id= self.request.id)
+
 
 ### Model for WKT arguments # WKT= 'POLYGON((30 10, 40 40, 20 40, 10 20, 30 10))'
